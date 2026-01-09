3b977f7559647663e5f44d2cff046ae598e17720
Emmanuelle Hemmer <hemmer.manu@gmail.com>
2026-01-09 17:40:17 +0100

    Enable Azure AD authentication in Terraform backend configuration

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "azurerm" {
     # Configuration fournie via -backend-config dans GitHub Actions
     # Les valeurs sont injectées depuis les secrets GitHub
+    use_azuread_auth = true
   }
 }
